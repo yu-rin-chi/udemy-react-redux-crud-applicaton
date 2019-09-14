5917719042844977f229e2eeaee47c1b738ae702
飯田 佑里 <yuri.iida@nijibox.co.jp>
2019-09-14 20:44:33 +0900

    JSX syntaxについて学んだ

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 //「スコープ」
 
 function App() {
-  return <input type="text" onClick={()=>{console.log("I am Clicked")}}/>;
+  return(
+    //  <React.Fragment> returnの内側を①つにまとめるdivのような役割
+    <React.Fragment> 
+      <label htmlFor="bar">
+        bar
+        </label>
+      <input type="text" onChange={()=>{console.log("I am Clicked")}}/>
+      </React.Fragment>
+  )
+  
+  
 }
 
 export default App;
