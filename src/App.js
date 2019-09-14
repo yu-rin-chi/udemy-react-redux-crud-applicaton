536f3c0bee962368bf1e7458b784e7e0be380985
飯田 佑里 <yuri.iida@nijibox.co.jp>
2019-09-14 22:27:57 +0900

    Componentのpropsについて学んだ。

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react'; 
 //「スコープ」
 
+
 const App =()=>{ //関数コンポーネント（functionalCOmponent）
+ 
+  const profiles =[
+  { name:"Taro", age:10 },
+  { name:"Hanako", age:5 },
+  { name:"Noname" }
+]  
   return (
   <div>
-    <User name={"Taro"} age={10}/>
-    <User name={"Hanako"} age={5}/>
+   {
+     profiles.map((profile,index)=> {
+     return <User name={profile.name} age={profile.age} key={index} />
+   })
+   }
   </div>
   )
 // Userに、nameというpropsが与えられ、そのデータはTaro
   }
- const User =(props)=>{
+
+const User =(props)=>{
 return <div>Hi, I am {props.name}, and {props.age}years old!</div> 
 // propsの中身の数だけ繰り返して描かれる
  }
 
+ User.defaultProps={
+   age:1
+ }
 export default App;
+
+// コンポーネントからコンポーネントへデータを受け渡す際にpropsを使う
